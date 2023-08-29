252ed3b9a9acb085951693c65b8fb0b447310026
evgeniy <pavlinbl4@gmail.com>
2023-08-29 11:49:25 +0300

    Добавлены комментарии строчкам кода

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 keyword_file = '/Users/evgeniy/Pictures/ExportKeywords_UTF8.TXT'
 keyword_file_edit = '/Users/evgeniy/Pictures/ExportKeywords_UTF8_edit.TXT'
 
+
+# Открываю исходный каталог ключевых слов
 with open(keyword_file, 'r') as source_file:
     lines = source_file.readlines()
-    print([lines[2]])
+
+    # Удаляю все невидимые символы табуляции переноса из слов
     striped_words = [word.strip() for word in lines]
+
+    # Запускаю цикл по количеству строк в файле
     for x in range(len(lines)):
+        #  count the number of occurrences of a specific word in the list of stripped words
         words_count = striped_words.count(lines[x].rstrip())
+
+        # Если в строке находится слово они синоним они категории, то оно начинается с буквы.
+        # Если слово начинается с буквы и содержится один раз списке слов то добав табудяцию, записываю его файл
         if lines[x][0].isalpha() and words_count == 1:
             lines[x] = '\t' + lines[x]
             print('\t' + lines[x].rstrip())
+    #  Если слово встречается больше чем один раз то в строчку ничего не записывает
+        # Странное решение так получается что стираю все двойные слова
         elif lines[x][0].isalpha() and words_count > 1:
+            print(f'word {lines[x].strip()} take place {words_count} in file')
             lines[x] = ''
 with open(keyword_file_edit, 'w') as edit_file:
     edit_file.writelines(lines)
